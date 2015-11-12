84cd4413dbddda05f007e2ae86bdeaafd53043b1
Natalie Groves <natalie.groves@postgrad.manchester.ac.uk>
2015-11-12 04:41:15 -0800

    tests sequence validity

--- lrg_parser.py
+++ lrg_parser.py
@@ -1,13 +1,20 @@
 import xml.etree.ElementTree as tree
 import sys
 import glob
+import re
 
 def test_version(root):
   assert root.attrib['schema_version'] == '1.9', 'Schema version must be 1.9'
+def check_sequence(sequence):
+  assert sequence is not None, 'sequence tag empty'
+  pattern = re.match('^[ATCG]+$', sequence)
+  assert pattern is not None, 'sequence not valid' 
+
 
 filenames = glob.glob('/home/swc/Desktop/lrg_parser/files_to_be_analysed/LRG*.xml')
 
 for file in filenames:
+  print file
   lrg_tree = tree.parse(file)
   root = lrg_tree.getroot()
   test_version(root)
@@ -19,5 +26,6 @@ for file in filenames:
   for tags in fa_children:
     if tags.tag == 'sequence':
       sequence = tags.text
-      print 'sequence found'
+      check_sequence(sequence)
+      print sequence[0:10]
     
